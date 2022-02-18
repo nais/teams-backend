85a1277b4a0af25275d968883706d18a7244e76d
Kim Tore Jensen <kimtjen@gmail.com>
2022-02-18 10:37:44 +0100

    Improve API documentation

--- pkg/apiserver/requests.go
+++ pkg/apiserver/requests.go
@@ -8,8 +8,10 @@ type GenericRequest struct {
 	ID string `json:"id" path:"id" format:"uuid"`
 }
 
+// This class is used to read both "id" from the URL and the team object from JSON.
 // Could have used models.Team, if not for the fact that "ID" (from URL)
-// must be read in through GenericRequest.
+// must be read in through GenericRequest, since having the "path" annotation
+// in models.Team generates bad documentation.
 type TeamRequest struct {
 	GenericRequest
 	models.Team
