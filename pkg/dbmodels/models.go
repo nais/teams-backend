516558dda07cf386e8047b53c2bb5ffe27e110b7
Christer Edvartsen <christer.edvartsen@nav.no>
2022-06-10 15:34:15 +0200

    Remove json tags as we don't convert the models to JSON anywhere.
    Also add gorm tags to all fields to have them all line up

--- pkg/dbmodels/models.go
+++ pkg/dbmodels/models.go
@@ -10,44 +10,44 @@ import (
 
 // Model Base model that all database tables inherit.
 type Model struct {
-	ID          *uuid.UUID `json:"id" gorm:"primaryKey; type:uuid; default:(uuid_generate_v4())"`
-	CreatedAt   time.Time  `json:"created_at" gorm:"<-:create; autoCreateTime; index; not null"`
-	CreatedBy   *User      `json:"-"`
-	UpdatedBy   *User      `json:"-"`
-	CreatedByID *uuid.UUID `json:"created_by_id" gorm:"type:uuid"`
-	UpdatedByID *uuid.UUID `json:"updated_by_id" gorm:"type:uuid"`
-	UpdatedAt   time.Time  `json:"updated_at" gorm:"autoUpdateTime; not null"`
+	ID          *uuid.UUID `gorm:"primaryKey; type:uuid; default:(uuid_generate_v4())"`
+	CreatedAt   time.Time  `gorm:"<-:create; autoCreateTime; index; not null"`
+	CreatedBy   *User      `gorm:""`
+	UpdatedBy   *User      `gorm:""`
+	CreatedByID *uuid.UUID `gorm:"type:uuid"`
+	UpdatedByID *uuid.UUID `gorm:"type:uuid"`
+	UpdatedAt   time.Time  `gorm:"autoUpdateTime; not null"`
 }
 
 // SoftDeletes Add fields to support Gorm soft deletes
 type SoftDeletes struct {
-	DeletedBy   *User          `json:"-"`
-	DeletedByID *uuid.UUID     `json:"deleted_by_id" gorm:"type:uuid"`
-	DeletedAt   gorm.DeletedAt `json:"-" gorm:"index"`
+	DeletedBy   *User          `gorm:""`
+	DeletedByID *uuid.UUID     `gorm:"type:uuid"`
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
 }
 
 type ApiKey struct {
 	Model
 	SoftDeletes
-	APIKey string    `json:"apikey" gorm:"unique; not null"`
-	User   *User     `json:"-"`
-	UserID uuid.UUID `json:"user_id" gorm:"type:uuid; not null"`
+	APIKey string    `gorm:"unique; not null"`
+	User   *User     `gorm:""`
+	UserID uuid.UUID `gorm:"type:uuid; not null"`
 }
 
 type AuditLog struct {
 	Model
 	SoftDeletes
-	System            *System
-	Synchronization   *Synchronization
-	User              *User // User object, not subject, i.e. which user was affected by the operation.
-	Team              *Team
-	SystemID          *uuid.UUID
-	SynchronizationID *uuid.UUID
-	UserID            *uuid.UUID
-	TeamID            *uuid.UUID
-	Success           bool   `gorm:"not null; index"` // True if operation succeeded
-	Action            string `gorm:"not null; index"` // CRUD action
-	Message           string `gorm:"not null"`        // User readable success or error message (log line)
+	System            *System          `gorm:""`
+	Synchronization   *Synchronization `gorm:""`
+	User              *User            `gorm:""` // User object, not subject, i.e. which user was affected by the operation.
+	Team              *Team            `gorm:""`
+	SystemID          *uuid.UUID       `gorm:""`
+	SynchronizationID *uuid.UUID       `gorm:""`
+	UserID            *uuid.UUID       `gorm:""`
+	TeamID            *uuid.UUID       `gorm:""`
+	Success           bool             `gorm:"not null; index"` // True if operation succeeded
+	Action            string           `gorm:"not null; index"` // CRUD action
+	Message           string           `gorm:"not null"`        // User readable success or error message (log line)
 }
 
 type RoleBinding struct {
@@ -82,7 +82,7 @@ type SystemState struct {
 	Model
 	Team        *Team
 	TeamID      *uuid.UUID `gorm:"uniqueIndex:system_env_team_key; index"`
-	System      *System
+	System      *System    `gorm:""`
 	SystemID    *uuid.UUID `gorm:"uniqueIndex:system_env_team_key; not null; index"`
 	Environment *string    `gorm:"uniqueIndex:system_env_team_key"`
 	Key         string     `gorm:"uniqueIndex:system_env_team_key; not null"`
@@ -97,48 +97,48 @@ type System struct {
 
 type SystemsTeams struct {
 	Model
-	System   *System    `json:"-" gorm:"not null"`
-	Team     *Team      `json:"-" gorm:"not null"`
-	SystemID *uuid.UUID `json:"system_id" gorm:"not null; index:systems_teams_index,unique"`
-	TeamID   *uuid.UUID `json:"team_id" gorm:"not null; index:systems_teams_index,unique"`
+	System   *System    `gorm:"not null"`
+	Team     *Team      `gorm:"not null"`
+	SystemID *uuid.UUID `gorm:"not null; index:systems_teams_index,unique"`
+	TeamID   *uuid.UUID `gorm:"not null; index:systems_teams_index,unique"`
 }
 
 type TeamMetadata struct {
 	Model
-	Team   *Team
+	Team   *Team      `gorm:""`
 	TeamID *uuid.UUID `gorm:"uniqueIndex:team_key"`
 	Key    string     `gorm:"uniqueIndex:team_key; not null"`
-	Value  *string
+	Value  *string    `gorm:""`
 }
 
 type Team struct {
 	Model
 	SoftDeletes
-	Slug        *Slug           `json:"slug" gorm:"<-:create; unique; not null"`
-	Name        *string         `json:"name" gorm:"unique; not null"`
-	Purpose     *string         `json:"purpose"`
-	Metadata    []*TeamMetadata `json:"-"`
-	SystemState []*SystemState  `json:"-"`
-	Users       []*User         `json:"-" gorm:"many2many:users_teams"`
-	Systems     []*System       `json:"-" gorm:"many2many:systems_teams"`
-	AuditLogs   []*AuditLog     `json:"-" gorm:"foreignKey:TeamID"`
+	Slug        *Slug           `gorm:"<-:create; unique; not null"`
+	Name        *string         `gorm:"unique; not null"`
+	Purpose     *string         `gorm:""`
+	Metadata    []*TeamMetadata `gorm:""`
+	SystemState []*SystemState  `gorm:""`
+	Users       []*User         `gorm:"many2many:users_teams"`
+	Systems     []*System       `gorm:"many2many:systems_teams"`
+	AuditLogs   []*AuditLog     `gorm:"foreignKey:TeamID"`
 }
 
 type User struct {
 	Model
 	SoftDeletes
-	Email        *string        `json:"email" gorm:"unique"`
-	Name         *string        `json:"name" gorm:"not null"`
-	Teams        []*Team        `json:"-" gorm:"many2many:users_teams"`
-	RoleBindings []*RoleBinding `json:"-" gorm:"foreignKey:UserID"`
+	Email        *string        `gorm:"unique"`
+	Name         *string        `gorm:"not null"`
+	Teams        []*Team        `gorm:"many2many:users_teams"`
+	RoleBindings []*RoleBinding `gorm:"foreignKey:UserID"`
 }
 
 type UsersTeams struct {
 	Model
-	Team   *Team      `json:"-" gorm:"not null"`
-	User   *User      `json:"-" gorm:"not null"`
-	UserID *uuid.UUID `json:"user_id" gorm:"not null; index:users_teams_index,unique"`
-	TeamID *uuid.UUID `json:"team_id" gorm:"not null; index:users_teams_index,unique"`
+	Team   *Team      `gorm:"not null"`
+	User   *User      `gorm:"not null"`
+	UserID *uuid.UUID `gorm:"not null; index:users_teams_index,unique"`
+	TeamID *uuid.UUID `gorm:"not null; index:users_teams_index,unique"`
 }
 
 // GetModel Enable callers to access the base model through an interface.
