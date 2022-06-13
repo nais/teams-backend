85911a0db1ca7c6bcb4b1562b4ceafc013c6fa87
Christer Edvartsen <christer.edvartsen@nav.no>
2022-06-13 14:27:52 +0200

    Return correct type

---
+++ pkg/graph/systems_test.go
@@ -0,0 +1,56 @@
+package graph_test
+
+import (
+	"context"
+	"github.com/nais/console/pkg/graph"
+	"github.com/nais/console/pkg/graph/model"
+	"github.com/nais/console/pkg/test"
+	"github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/nais/console/pkg/dbmodels"
+)
+
+func TestQueryResolver_Systems(t *testing.T) {
+	db := test.GetTestDB()
+	db.AutoMigrate(&dbmodels.System{})
+	db.Create([]dbmodels.System{
+		{
+			Name: "B",
+		},
+		{
+			Name: "A",
+		},
+		{
+			Name: "C",
+		},
+	})
+
+	ch := make(chan *dbmodels.Team, 100)
+	system := getSystem()
+	ctx := context.Background()
+	resolver := graph.NewResolver(db, system, ch, nil).Query()
+
+	t.Run("No filter or sort", func(t *testing.T) {
+		systems, err := resolver.Systems(ctx, nil, nil, nil)
+		assert.NoError(t, err)
+
+		assert.Len(t, systems.Nodes, 3)
+		assert.Equal(t, "A", systems.Nodes[0].Name)
+		assert.Equal(t, "B", systems.Nodes[1].Name)
+		assert.Equal(t, "C", systems.Nodes[2].Name)
+	})
+
+	t.Run("Sort name DESC", func(t *testing.T) {
+		systems, err := resolver.Systems(ctx, nil, nil, &model.SystemsSort{
+			Field:     model.SystemSortFieldName,
+			Direction: model.SortDirectionDesc,
+		})
+		assert.NoError(t, err)
+
+		assert.Len(t, systems.Nodes, 3)
+		assert.Equal(t, "C", systems.Nodes[0].Name)
+		assert.Equal(t, "B", systems.Nodes[1].Name)
+		assert.Equal(t, "A", systems.Nodes[2].Name)
+	})
+}
