e146e3bd4b2e47020dde11e1af435daf1d34b033
Christer Edvartsen <christer.edvartsen@nav.no>
2022-06-22 10:53:53 +0200

    Simplify the usage of system state

--- pkg/dbmodels/system_state.go
+++ pkg/dbmodels/system_state.go
@@ -6,9 +6,12 @@ import (
 	"gorm.io/gorm"
 )
 
-// GetSystemState Get the team state for a given system. If the team has no state in the system yet, a blank one will be
-// created, and then returned.
-func GetSystemState(db *gorm.DB, systemId, teamId uuid.UUID) (*SystemState, error) {
+type UpdateStateFunc func(newState interface{}) error
+
+// LoadSystemState Fetch the team state for a given system, and load the current state into the passed state parameter.
+// If the team has no state in the system yet, a blank one will be createdThe UpdateStateFunc return function can be
+// used to update the state if needed.
+func LoadSystemState(db *gorm.DB, systemId, teamId uuid.UUID, state interface{}) (UpdateStateFunc, error) {
 	systemState := &SystemState{
 		SystemID: systemId,
 		TeamID:   teamId,
@@ -19,5 +22,23 @@ func GetSystemState(db *gorm.DB, systemId, teamId uuid.UUID) (*SystemState, erro
 		return nil, fmt.Errorf("get system state: %w", err)
 	}
 
-	return systemState, nil
+	err = systemState.State.AssignTo(&state)
+	if err != nil {
+		return nil, fmt.Errorf("unable to assign state: %w", err)
+	}
+
+	return func(newState interface{}) error {
+		state = newState
+		err = systemState.State.Set(newState)
+		if err != nil {
+			return fmt.Errorf("system state not set: %w", err)
+		}
+
+		err = db.Save(systemState).Error
+		if err != nil {
+			return fmt.Errorf("system state not persisted: %w", err)
+		}
+
+		return nil
+	}, nil
 }
