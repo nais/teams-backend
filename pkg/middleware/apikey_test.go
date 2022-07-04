4c0f16c87ced84430a5bcddf6c222dc11aa550cb
Christer Edvartsen <christer.edvartsen@nav.no>
2022-07-04 11:02:21 +0200

    Add test for api key middleware

--- pkg/middleware/apikey_test.go
+++ pkg/middleware/apikey_test.go
@@ -1,34 +1,66 @@
 package middleware
 
 import (
+	"github.com/nais/console/pkg/authz"
 	"github.com/nais/console/pkg/test"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/nais/console/pkg/dbmodels"
-	"gorm.io/gorm"
 )
 
-func setupFixtures(db *gorm.DB) error {
-	db.AutoMigrate(&dbmodels.User{})
-	return db.Transaction(func(tx *gorm.DB) error {
-		tx.Create(&dbmodels.User{
-			Model:      dbmodels.Model{},
-			SoftDelete: dbmodels.SoftDelete{},
-			Email:      "user@example.com",
-			Name:       "User Name",
-			Teams:      nil,
-		})
-		return nil
-	})
+func getRequest() *http.Request {
+	req, _ := http.NewRequest(http.MethodPost, "/query", nil)
+	return req
 }
 
 func TestApiKeyAuthentication(t *testing.T) {
 	db := test.GetTestDB()
-	err := setupFixtures(db)
-	if err != nil {
-		panic(err)
-	}
+	db.AutoMigrate(&dbmodels.ApiKey{}, &dbmodels.User{})
+	user1 := &dbmodels.User{Email: "user1@example.com"}
+	user2 := &dbmodels.User{Email: "user2@example.com"}
+	user3 := &dbmodels.User{Email: "user3@example.com"}
+	db.Create([]*dbmodels.User{user1, user2, user3})
 
-	// FIXME: Do some actual testing
-	_ = db
+	apiKey1 := &dbmodels.ApiKey{APIKey: "user1-key", UserID: *user1.ID}
+	apiKey2 := &dbmodels.ApiKey{APIKey: "user2-key", UserID: *user2.ID}
+	db.Create([]*dbmodels.ApiKey{apiKey1, apiKey2})
+
+	middleware := ApiKeyAuthentication(db)
+	responseWriter := httptest.NewRecorder()
+
+	t.Run("No authorization header", func(t *testing.T) {
+		next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			user := authz.UserFromContext(r.Context())
+			assert.Nil(t, user)
+		})
+
+		req := getRequest()
+		middleware(next).ServeHTTP(responseWriter, req)
+	})
+
+	t.Run("Unknown API key in header", func(t *testing.T) {
+		next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			user := authz.UserFromContext(r.Context())
+			assert.Nil(t, user)
+		})
+
+		req := getRequest()
+		req.Header.Set("Authorization", "Bearer unknown")
+		middleware(next).ServeHTTP(responseWriter, req)
+	})
+
+	t.Run("Valid API key", func(t *testing.T) {
+		next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			user := authz.UserFromContext(r.Context())
+			assert.NotNil(t, user)
+			assert.Equal(t, "user1@example.com", user.Email)
+		})
+
+		req := getRequest()
+		req.Header.Set("Authorization", "Bearer user1-key")
+		middleware(next).ServeHTTP(responseWriter, req)
+	})
 }
