274738a6d15625649bd72ad3fe38300cdef18012
Christer Edvartsen <christer.edvartsen@nav.no>
2022-07-05 14:11:37 +0200

    Fix typo

--- pkg/roles/roles.go
+++ pkg/roles/roles.go
@@ -3,11 +3,11 @@ package roles
 type Role string
 
 const (
-	RoleAdmin                  Role = "Admin"
-	RoleServiceAccountCreaetor Role = "Service account creator"
-	RoleServiceAccountOwner    Role = "Service account owner"
-	RoleTeamCreator            Role = "Team creator"
-	RoleTeamMember             Role = "Team member"
-	RoleTeamOwner              Role = "Team owner"
-	RoleTeamViewer             Role = "Team viewer"
+	RoleAdmin                 Role = "Admin"
+	RoleServiceAccountCreator Role = "Service account creator"
+	RoleServiceAccountOwner   Role = "Service account owner"
+	RoleTeamCreator           Role = "Team creator"
+	RoleTeamMember            Role = "Team member"
+	RoleTeamOwner             Role = "Team owner"
+	RoleTeamViewer            Role = "Team viewer"
 )
