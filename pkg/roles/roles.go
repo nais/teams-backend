95b8056de5e1b397810a576439f23e78adc7befa
Christer Edvartsen <christer.edvartsen@nav.no>
2022-07-07 17:46:01 +0200

    Add consts

--- pkg/roles/roles.go
+++ pkg/roles/roles.go
@@ -1,8 +1,24 @@
 package roles
 
+type Authorization string
 type Role string
 
 const (
+	AuthorizationAuditLogsRead         Authorization = "audit_logs.read"
+	AuthorizationServiceAccountsCreate Authorization = "service_accounts.create"
+	AuthorizationServiceAccountsDelete Authorization = "service_accounts.delete"
+	AuthorizationServiceAccountList    Authorization = "service_accounts.list"
+	AuthorizationServiceAccountsUpdate Authorization = "service_accounts.update"
+	AuthorizationSystemStatesDelete    Authorization = "system_states.delete"
+	AuthorizationSystemStatesRead      Authorization = "system_states.read"
+	AuthorizationSystemStatesUpdate    Authorization = "system_states.update"
+	AuthorizationTeamsCreate           Authorization = "teams.create"
+	AuthorizationTeamsDelete           Authorization = "teams.delete"
+	AuthorizationTeamsList             Authorization = "teams.list"
+	AuthorizationTeamsRead             Authorization = "teams.read"
+	AuthorizationTeamsUpdate           Authorization = "teams.update"
+	AuthorizationUsersList             Authorization = "users.list"
+
 	RoleAdmin                 Role = "Admin"
 	RoleServiceAccountCreator Role = "Service account creator"
 	RoleServiceAccountOwner   Role = "Service account owner"
@@ -10,4 +26,5 @@ const (
 	RoleTeamMember            Role = "Team member"
 	RoleTeamOwner             Role = "Team owner"
 	RoleTeamViewer            Role = "Team viewer"
+	RoleUserViewer            Role = "User viewer"
 )
