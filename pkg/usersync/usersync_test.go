87982bcb3f0c771dff0347874f5123429ba28e1e
Christer Edvartsen <christer.edvartsen@nav.no>
2022-08-16 09:08:44 +0200

    Add test case to trigger the OnConflict clause when adding default roles

--- pkg/usersync/usersync_test.go
+++ pkg/usersync/usersync_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/nais/console/pkg/auditlogger"
 	"github.com/nais/console/pkg/dbmodels"
+	"github.com/nais/console/pkg/roles"
 	"github.com/nais/console/pkg/test"
 	"github.com/nais/console/pkg/usersync"
 	"github.com/stretchr/testify/assert"
@@ -74,6 +75,33 @@ func TestSync(t *testing.T) {
 		mockAuditLogger.
 			On("Logf", usersync.OpDelete, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.MatchedBy(func(targetUser *dbmodels.User) bool {
 				return targetUser.Name == "Delete Me"
+			}), mock.Anything).
+			Return(nil).
+			Once()
+
+		usersync := usersync.New(db, *system, mockAuditLogger, "example.com", httpClient)
+		err := usersync.Sync(context.Background())
+		assert.NoError(t, err)
+		mockAuditLogger.AssertExpectations(t)
+	})
+
+	t.Run("Handle conflict for user roles", func(t *testing.T) {
+		db, _ := test.GetTestDBWithRoles()
+		db.Create(system)
+		user := &dbmodels.User{Email: "user@example.com"}
+		role := &dbmodels.Role{}
+		db.Where("name = ?", roles.RoleTeamCreator).First(role)
+		db.Create([]*dbmodels.User{user})
+		db.Create(&dbmodels.UserRole{RoleID: *role.ID, UserID: *user.ID})
+
+		httpClient := test.NewTestHttpClient(func(req *http.Request) *http.Response {
+			return test.Response("200 OK", `{"users":[`+
+				`{"primaryEmail":"user@example.com","name":{"fullName":"User Name"}}]}`) // Will be created
+		})
+
+		mockAuditLogger.
+			On("Logf", usersync.OpUpdate, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.MatchedBy(func(targetUser *dbmodels.User) bool {
+				return targetUser.Name == "User Name"
 			}), mock.Anything).
 			Return(nil).
 			Once()
