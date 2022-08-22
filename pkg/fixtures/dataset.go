1072748def5ed40e941414c4c39307e1a3f761fa
Christer Edvartsen <christer.edvartsen@nav.no>
2022-08-22 16:53:16 +0200

    Create initial admin and API key
    Will be moved to DB migrations later on

--- pkg/fixtures/dataset.go
+++ pkg/fixtures/dataset.go
@@ -2,6 +2,7 @@ package fixtures
 
 import (
 	"context"
+	"github.com/nais/console/pkg/sqlc"
 
 	"github.com/nais/console/pkg/db"
 )
@@ -11,20 +12,26 @@ const (
 	AdminUserEmailPrefix = "nais-console" // matches the default nais admin user account in the tenant GCP org
 )
 
-// InsertInitialDataset Insert an initial dataset into the database. This will only be executed if there are currently
-// no users in the users table.
+// InsertInitialDataset Insert an initial dataset into the database
 func InsertInitialDataset(ctx context.Context, database db.Database, tenantDomain string, adminApiKey string) error {
 	if adminApiKey != "" {
-		admin, err := database.GetUserByEmail(ctx, AdminUserEmailPrefix+"@"+tenantDomain)
+		_, err := database.GetUserByEmail(ctx, AdminUserEmailPrefix+"@"+tenantDomain)
 		if err != nil {
-			return err
-		}
+			admin, err := database.AddUser(ctx, AdminUserName, AdminUserEmailPrefix+"@"+tenantDomain)
+			if err != nil {
+				return err
+			}
 
-		err = database.CreateAPIKey(ctx, adminApiKey, admin.ID)
-		if err != nil {
-			return err
-		}
+			err = database.CreateAPIKey(ctx, adminApiKey, admin.ID)
+			if err != nil {
+				return err
+			}
 
+			err = database.AssignGlobalRoleToUser(ctx, admin.ID, sqlc.RoleNameAdmin)
+			if err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
