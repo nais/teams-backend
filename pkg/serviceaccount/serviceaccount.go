0fd762c4a783b686ed7d3593c65e208318f0f6eb
Christer Edvartsen <christer.edvartsen@nav.no>
2022-08-23 14:29:36 +0200

    Add missing package

---
+++ pkg/serviceaccount/serviceaccount.go
@@ -0,0 +1,22 @@
+package serviceaccount
+
+import (
+	"github.com/nais/console/pkg/db"
+	"github.com/nais/console/pkg/slug"
+	"strings"
+)
+
+// Email Generate a service account email address given the name of the service account
+func Email(name slug.Slug, tenantDomain string) string {
+	return string(name) + serviceAccountSuffix(tenantDomain)
+}
+
+// IsServiceAccount Check if a user is a service account or not
+func IsServiceAccount(user db.User, tenantDomain string) bool {
+	return strings.HasSuffix(user.Email, serviceAccountSuffix(tenantDomain))
+}
+
+func serviceAccountSuffix(tenantDomain string) string {
+	const serviceAccountSuffix = "serviceaccounts.nais.io"
+	return "@" + tenantDomain + "." + serviceAccountSuffix
+}
