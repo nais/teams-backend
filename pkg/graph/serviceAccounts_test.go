f26a653b76bb1c7f0a42b1a96c60e2a9e6c11fd0
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2022-11-11 13:45:45 +0100

    test for serviceaccount resolver

---
+++ pkg/graph/serviceAccounts_test.go
@@ -0,0 +1,54 @@
+package graph_test
+
+import (
+	"context"
+	"github.com/nais/console/pkg/auditlogger"
+	"github.com/nais/console/pkg/graph"
+	"github.com/nais/console/pkg/reconcilers"
+	"github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/nais/console/pkg/authz"
+	"github.com/nais/console/pkg/db"
+	"github.com/nais/console/pkg/sqlc"
+)
+
+func TestMutationResolver_Roles(t *testing.T) {
+	serviceAccount := &db.ServiceAccount{
+		ServiceAccount: &sqlc.ServiceAccount{
+			ID:   uuid.New(),
+			Name: "User Name",
+		},
+	}
+	ctx := authz.ContextWithActor(context.Background(), serviceAccount, []*db.Role{
+		{
+			RoleName: sqlc.RoleNameAdmin,
+			Authorizations: []sqlc.AuthzName{
+				sqlc.AuthzNameTeamsCreate,
+			},
+		},
+	})
+
+	reconcilers := make(chan reconcilers.Input, 100)
+	auditLogger := auditlogger.NewMockAuditLogger(t)
+	database := db.NewMockDatabase(t)
+	resolver := graph.NewResolver(database, "example.com", reconcilers, auditLogger).ServiceAccount()
+
+	t.Run("get roles for serviceAccount", func(t *testing.T) {
+
+		role := &db.Role{
+			Authorizations:         nil,
+			RoleName:               "",
+			TargetServiceAccountID: nil,
+			TargetTeamSlug:         nil,
+		}
+
+		database.On("GetServiceAccountRoles", ctx, serviceAccount.ID).
+			Return([]*db.Role{role}, nil)
+
+		roles, err := resolver.Roles(ctx, serviceAccount)
+		assert.NoError(t, err)
+		assert.Equal(t, roles[0], role)
+	})
+}
