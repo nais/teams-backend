114ef0bb506ade80c925128b07c16474215c726e
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2023-01-03 14:27:51 +0100

    more typos

--- sqlc/schemas/0035_add_synchronizer.down.sql
+++ sqlc/schemas/0035_add_synchronizer.down.sql
@@ -3,10 +3,10 @@ BEGIN;
 /* PostgreSQL does not support removing single items from an ENUM, one must DROP the type, and re-create it */
 
 DELETE FROM role_authz WHERE
-  (role_name='Team owner' AND authz_name='team:synchronize') OR
-  (role_name='Synchronizer' AND authz_name='team:synchronize') OR
+  (role_name='Team owner' AND authz_name='teams:synchronize') OR
+  (role_name='Synchronizer' AND authz_name='teams:synchronize') OR
   (role_name='Synchronizer' AND authz_name='usersync:synchronize') OR
-  (role_name='Admin' AND authz_name='team:synchronize') OR
+  (role_name='Admin' AND authz_name='teams:synchronize') OR
   (role_name='Admin' AND authz_name='usersync:synchronize')
     ;
 
